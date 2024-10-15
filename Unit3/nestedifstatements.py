b90f64fc603c744008a6a3e3b91bc8dfcf80456b
BEAURUETER10 <830665@mystma.org>
2024-10-15 15:06:58 -0500

    shipping

---
+++ Unit3/nestedifstatements.py
@@ -0,0 +1,30 @@
+# nested if statements
+
+
+prime = True
+cost = 20
+age = 17 
+consent = False 
+
+if prime:
+    if age >= 18:
+        print(" FREE SHIPPING APPLIED!")
+    elif consent: 
+        print("FREE SHIPPING APPLIED")
+elif:
+        print("no free ship for you")
+
+
+elif cost >= 25:
+    if age >=18:
+        print("")
+
+
+
+
+
+raining = input("Is it raining outside?\n>")
+outside = input("Do you plan on going outside?\n>")
+
+if raining.lower() == "yes":
+    outside = input("Do you plan on going outside?\n>")
--- assignments/freeshipping.py
+++ assignments/freeshipping.py
@@ -3,3 +3,14 @@
 # you only get free shipping if 
 # you are a prime member OR order is at least $25
 # you are at least 18 years old OR have parent 
+
+
+def check_eligibility(age):
+    if age >= 18:
+        print("you qualify for free shipping")
+    
+    else:
+        print("you do not qualify for free shipping")
+
+
+check_eligibility()
