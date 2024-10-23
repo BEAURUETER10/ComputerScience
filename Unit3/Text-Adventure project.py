0f531a3fa8a3b21cb15b7a286470089cf242d8f4
BEAURUETER10 <830665@mystma.org>
2024-10-23 15:16:31 -0500

    Spaghetti

--- Unit3/Text-Adventure project.py
+++ Unit3/Text-Adventure project.py
@@ -25,13 +25,40 @@ def stay_home():
 def go_out():
     print("You go out on the street and walk to the supermarket ")
     print("1. Go in")
-    print("2. Keep going")
+    print("2. Keep going ")
+# end of frist interaction
+
+
+
 
     choice = input(">")
 
     if choice == "1":
         go_in()
     elif choice == "2":
-        keep_going()
+        go_home()
+    else: print("Invalid choice. Try again ")
+
+def go_in():
+    print("You go in the supermarket. what do you get? ")
+    print("1. egg, milk, flour, and cocoa ")
+    print("2. tomato sauce, Spaghetti, ground beef, and parmesan cheese ")
 
 
+def go_home():
+    print("you go home. NOT TODAY ENDING")
+# end of 2 interaction
+
+    choice = input(">")
+    if choice == "1":
+        egg_milk_flour_cocoa()
+    elif choice == "2":
+        tomato_sauce_Spaghetti_ground_beef_parmesan_cheese()
+    else:
+        print("Invalid choice. Try again ")
+
+def egg_milk_flour_cocoa():
+    print("")
+
+def tomato_sauce_Spaghetti_ground_beef_parmesan_cheese():
+    print("")
