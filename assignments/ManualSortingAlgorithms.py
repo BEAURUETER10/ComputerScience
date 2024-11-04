a6fd1b715d7b7bee364a6c048b0f4fa418713c7f
BEAURUETER10 <830665@mystma.org>
2024-11-04 15:13:00 -0600

    manual

---
+++ assignments/ManualSortingAlgorithms.py
@@ -0,0 +1,24 @@
+
+import random
+
+numbers = [random.randint(0,101), random.randint(0,101), random.randint(0,101), random.randint(0,101), random.randint(0,101)]
+
+print(numbers)
+
+def bubble_sort(n):
+
+    steps = 0
+
+    for i in range (0, len(n)-1):
+        if n[i]> n[i+1]:
+            n[i], n[i+1] = n[i+1], n[i]
+           
+        steps += 1
+
+    print(n)
+
+    print(str(steps) + "steps to complete.")
+
+
+
+
