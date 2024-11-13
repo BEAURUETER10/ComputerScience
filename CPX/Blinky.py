cff64dea2a80a04a6e8bb431fd9421efb840589a
BEAURUETER10 <830665@mystma.org>
2024-11-13 15:17:40 -0600

    it

--- CPX/Blinky.py
+++ CPX/Blinky.py
@@ -1,14 +1,12 @@
 from adafruit_circuitplayground import cp
 
 
-
-
 while True:
     cp.pixels.fill((0,5,0))
 
     import time
-    print()
-    time.sleep(0.367)
-    print()
+  
+    time.sleep(0.5)
+    
         
         
