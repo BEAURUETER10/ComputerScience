0671a70797781df52d42b34df44d6a55e0c31301
BEAURUETER10 <830665@mystma.org>
2024-11-14 11:26:16 -0600

    pass

--- CPX/Blinky.py
+++ CPX/Blinky.py
@@ -1,12 +1,25 @@
 from adafruit_circuitplayground import cp
-
-
-while True:
-    cp.pixels.fill((0,5,0))
-
-    import time
-  
-    time.sleep(0.5)
-    
+import time
+cp.pixels.brightness = 0.1
+while True: 
+    cp.pixels.fill((0,255,0))
+    time.sleep(0.367)
+    cp.pixels.fill((0,0,0))
+    time.sleep(0.367)
         
-        
+
+
+
+
+
+from adafruit_circuitplayground import cp
+import time
+
+while True: 
+   if cp.button_a:
+      for i in range(0,10):
+            cp.pixels[i] = (0,225,0)
+            time.sleep(0.100)
+            cp.pixels[i] = (0,0,0)  
+            while cp.button_a:
+                pass
