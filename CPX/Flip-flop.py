56cb68e115648643c1d890ff84cd3936821b3ded
BEAURUETER10 <830665@mystma.org>
2024-11-21 11:25:29 -0600

    Switch

--- CPX/Flip-flop.py
+++ CPX/Flip-flop.py
@@ -2,7 +2,27 @@ from adafruit_circuitplayground import cp
 
 
 while True:
-    if cp.switch == False:
-        cp.pixels.fill[0] = (0, 0, 0)
-        cp.pixels.fill[1] = (0, 0, 0)
-                                                                                                                                                                            
+    if cp.switch == True:
+        cp.pixels[0] = (0, 25, 0)
+        cp.pixels[1] = (0, 25, 0)
+        cp.pixels[2] = (0, 25, 0)
+        cp.pixels[3] = (0, 25, 0)
+        cp.pixels[4] = (0, 25, 0)
+
+        cp.pixels[9] = (0, 0, 0)
+        cp.pixels[8] = (0, 0, 0)  
+        cp.pixels[7] = (0, 0, 0)
+        cp.pixels[6] = (0, 0, 0)
+        cp.pixels[5] = (0, 0, 0)
+    else:
+        cp.pixels[9] = (0, 25, 0)
+        cp.pixels[8] = (0, 25, 0)  
+        cp.pixels[7] = (0, 25, 0)
+        cp.pixels[6] = (0, 25, 0)
+        cp.pixels[5] = (0, 25, 0)
+    
+        cp.pixels[0] = (0, 0, 0)
+        cp.pixels[1] = (0, 0, 0)
+        cp.pixels[2] = (0, 0, 0)
+        cp.pixels[3] = (0, 0, 0)
+        cp.pixels[4] = (0, 0, 0)
